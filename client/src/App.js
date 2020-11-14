bb6dbdfc35b3cdb0a50a79721fc5c644f0b9fef8
Michael Pieper <michaelepieper@gmail.com>
2020-11-13 21:46:19 -0600

    Set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Navbar from "./components/Navbar/index";
+import NewIdea from "./views/NewIdea";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+          <Navbar />
+          <Switch>
+              <Route exact={true} path="/new-idea" component={NewIdea}/>
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/views/NewIdea/index.js
+++ client/src/views/NewIdea/index.js
@@ -3,16 +3,27 @@ import "./NewIdea.scss";
 
 class NewIdea extends Component {
 
-    constructor() {
-        super(props);
-    }
-
     render() {
-        return <div className="page-container">
-            <input placeholder="My New Idea"/>
-            <h2>Upload Images</h2>
-        </div>
-    ;
+        return (
+            <div className="page-container">
+                <div className="left-container">
+                    <input placeholder="My New Idea"/>
+                    <h2>Upload Images</h2>
+                </div>
+                <div className="right-container">
+                    <button>SUBMIT</button>
+                    <div>
+                        <input type="checkbox" name="public"/>
+                        <label for="public">Viewable to others</label>
+                    </div>
+                    <select>
+                        <option>Test 1</option>
+                        <option>Test 2</option>
+                        <option>Test 3</option>
+                    </select>
+                </div>
+            </div>
+        );
     }
 
 }
