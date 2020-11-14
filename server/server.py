2d3728b964383fd0e330de45913db7074118abc6
Josh Guempel <joshguempel5@gmail.com>
2020-11-14 11:42:55 -0600

    API functioning

--- server/server.py
+++ server/server.py
@@ -1,47 +1,51 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
-
+import json
 app = Flask(__name__)
 
-topics = [
-  "Feed",
-  "Pickles",
-  "Apps",
-  "Sports",
-  "Tech",
-  "Music",
-  "Borgor Maker",
-  "Kanye West",
-  "Cult",
-]
+class Idea:
+    def __init__(self, topic, title, description, visible, author):
+        self.topic = topic
+        self.title = title
+        self.description = description
+        self.visible = visible
+        self.author = author
+
+    def serial(self):
+        return {
+            'topic': self.topic,
+            'title': self.title,
+            'description': self.description,
+            'visible': self.visible,
+            'author': self.author
+        }
 
 ideas = {}
 
 @app.route('/ideas/<topic>', methods = ['GET'])
 def get_ideas(topic):
-    return jsonify({
-        'topic': topic,
-        'ideas': {
-            'game idea 1': 'desc',
-            'game idea 2': 'desc'
-        }
-    })
+    return jsonify(ideas=[idea.serial() for idea in ideas[topic]])
 
 @app.route('/ideas/<topic>', methods = ['POST'])
 def post_idea(topic):
     title = request.args.get('title')
     description = request.args.get('description')
+    visible = request.args.get('visible')
+    author = request.args.get('author')
+    idea = Idea(topic, title, description, visible, author)
+    
+    print("###########################################")
+    print(idea.topic, idea.title, idea.description, idea.visible, idea.author)
+    print("###########################################")
 
-    print(title, description)
+    if topic in ideas:
+        ideas[topic].append(idea)
+    else:
+        ideas[topic] = [idea]
 
-    return jsonify(
-        topic=topic,
-        title=title,
-        description=description
-    )
+    return json.dumps(idea.__dict__)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    for topic in topics:
-        ideas[topic] = {}
+    
