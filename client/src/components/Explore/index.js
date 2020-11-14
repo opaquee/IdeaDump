53c75aec4ad26703c3ff480a941c6df38275c6d4
Henri Evjen <henrievjen@Henris-MacBook-Pro.local>
2020-11-14 10:30:37 -0600

    added topics buttons

--- client/src/components/Explore/index.js
+++ client/src/components/Explore/index.js
@@ -1,4 +1,5 @@
 import "./Explore.scss";
+import React, { useState } from "react";
 
 const topics = [
   "Feed",
@@ -11,14 +12,35 @@ const topics = [
   "Kanye West",
   "Cult",
 ];
-const topicsDiv = topics.map((topic) => {
-  return <button className="topic">{topic}</button>;
-});
 
 function Explore() {
+  const [active, setActive] = useState("Feed");
+
+  const activeClick = (topic) => {
+    setActive(topic);
+  };
+
+  const topicsDiv = topics.map((topic) => {
+    return (
+      <button
+        className={active === topic ? "active topic" : "topic"}
+        onClick={() => {
+          activeClick(topic);
+        }}
+      >
+        {topic}
+      </button>
+    );
+  });
+
   return (
     <div className="container">
+      <div className="header">
+        <h1 className="explore-h1">Explore</h1>
+        <p>Find the perfect idea.</p>
+      </div>
       <div className="topics-container">{topicsDiv}</div>
+      <input type="text" className="search-bar" placeholder="Search" />
     </div>
   );
 }
