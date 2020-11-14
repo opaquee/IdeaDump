f2c4f6e386b0220b5061ecf11b30e53177791eb5
Henri Evjen <henrievjen@Henris-MacBook-Pro.local>
2020-11-13 22:42:05 -0600

    added navbar routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
 import NewIdea from "./views/NewIdea";
+import Explore from "./components/Explore";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-          <Navbar />
-          <Switch>
-              <Route exact={true} path="/new-idea" component={NewIdea}/>
-          </Switch>
+        <Navbar />
+        <Switch>
+          <Route exact={true} path="/">
+            <Redirect to="/explore" />
+          </Route>
+          <Route exact={true} path="/new-idea" component={NewIdea} />
+          <Route exact={true} path="/explore" component={Explore} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
